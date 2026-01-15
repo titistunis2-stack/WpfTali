021bf0152e3d34b37f400846e9c3ec645c8d35bc
titistunis2 <titistunis2@gmail.com>
2026-01-15 10:43:40 +0200

    design

--- RegistrationPage.xaml.cs
+++ RegistrationPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,15 +16,13 @@ using System.Windows.Shapes;
 namespace WpfTali
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RegistrationPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistrationPage : Page
     {
-        public MainWindow()
+        public RegistrationPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new LoginPage());
-          
         }
     }
 }
